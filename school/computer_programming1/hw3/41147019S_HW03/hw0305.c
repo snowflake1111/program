f36e94a7fe666db8a230bd1b3fef1eebf80e78b5
SF <66545038+snowflake1111@users.noreply.github.com>
2022-11-13 00:21:53 +0800

    Update hw0305.c

--- school/computer_programming1/hw3/41147019S_HW03/hw0305.c
+++ school/computer_programming1/hw3/41147019S_HW03/hw0305.c
@@ -3,6 +3,9 @@
 #include "logic.h"
 #include "smallten.h"
 
+void multiplier(bool x0, bool x1, bool x2, bool x3, bool y0, bool y1, bool y2, bool y3);
+void print(bool x0, bool x1, bool x2, bool x3, bool y0, bool y1, bool y2, bool y3);
+
 int main(){
     int32_t a = 0;
     int32_t b = 0;
@@ -44,4 +47,38 @@ int main(){
     
     print(x0, x1, x2, x3, y0, y1, y2, y3);
 }
+
+void multiplier(bool x0, bool x1, bool x2, bool x3, bool y0, bool y1, bool y2, bool y3){
+    z0 = andGate(x0, y0);
+    z1 = andGate(x1, y0);
+    z2 = andGate(x2, y0);
+    z3 = andGate(x3, y0);
+    
+    z1 = halfAdder(z1, andGate(x0, y1));
+    z2 = fullAdder(z2, andGate(x1, y1), tmpC);
+    z3 = fullAdder(z3, andGate(x2, y1), cOut);
+    z4 = halfAdder(andGate(x3, y1), cOut);
+    z5 = tmpC;
+
+    z2 = halfAdder(z2, andGate(x0, y2));
+    z3 = fullAdder(z3, andGate(x1, y2), tmpC);
+    z4 = fullAdder(z4, andGate(x2, y2), cOut);
+    z5 = fullAdder(z5, andGate(x3, y2), cOut);
+    z6 = cOut;
+
+    z3 = halfAdder(z3, andGate(x0, y3));
+    z4 = fullAdder(z4, andGate(x1, y3), tmpC);
+    z5 = fullAdder(z5, andGate(x2, y3), cOut);
+    z6 = fullAdder(z6, andGate(x3, y3), cOut);
+    z7 = cOut;
+
+    return;
+}
+
+void print(bool x0, bool x1, bool x2, bool x3, bool y0, bool y1, bool y2, bool y3){
+    printf("A = %d%d%d%d (2) = %d (10)\n", x3, x2, x1, x0, x3 * 8 + x2 * 4 + x1 * 2 + x0);
+    printf("B = %d%d%d%d (2) = %d (10)\n", y3, y2, y1, y0, y3 * 8 + y2 * 4 + y1 * 2 + y0);
+    printf("A x B = %d * %d = %d%d%d%d%d%d%d%d (2) = %d (10)\n", x3 * 8 + x2 * 4 + x1 * 2 + x0, y3 * 8 + y2 * 4 + y1 * 2 + y0, z7, z6, z5, z4, z3, z2, z1, z0, (x3 * 8 + x2 * 4 + x1 * 2 + x0) * (y3 * 8 + y2 * 4 + y1 * 2 + y0));
+    
+    return;
     
