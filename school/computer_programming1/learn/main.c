81f848750ca01c39ccad3ab6cd456af9292533c9
snowflake <snowflake111192@gmail.com>
2022-11-23 21:50:36 +0800

    2022.11.23

--- school/computer_programming1/learn/main.c
+++ school/computer_programming1/learn/main.c
@@ -1,11 +1,75 @@
 #include <stdio.h>
-#include "myfunction.h"
+//#include "myfunction.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+void sort(int32_t array[], size_t n);
+void mean(const int32_t array[], size_t n);
+void median(const int32_t array[], size_t n);
 
 int main(){
-    int32_t a = 1, b = 2, c = 0;
-    c = add_function(a, b);
-    printf("%d\n", c);
+    int32_t array[100] = {0};
+    int32_t n = 0;
+
+    srand(time(0));
+
+    scanf("%d", &n);
+
+    for(int32_t i=0 ; i<n ; i++){
+        array[i] = rand() % 10 + 1;
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+
+    sort(array, n);
+    
+    for(int32_t i=0 ; i<n ; i++){
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+    
+    mean(array, n);
+    median(array, n);
 
     return 0;
+}
+
+void sort(int32_t array[], size_t n){
+    for(int32_t i=0 ; i<n ; i++){
+        for(int32_t j=i+1 ; j<n ; j++){
+            if(array[j] < array[i]){
+                int32_t temp = 0;
+
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+void median(const int32_t array[], size_t n){
+    if(n % 2 == 0){
+        printf("median: %lf\n", (double) (array[n/2-1] + array[n/2]) / 2);    
+    }
+    else{
+        printf("median: %lf\n", (double) array[(n+1)/2-1]);
+    }
+
+    return;
+}
+
+void mean(const int32_t array[], size_t n){
+    int64_t sum = 0;
+    
+    for(int32_t i=0 ; i<n ; i++){
+        sum = sum + array[i];
+    }
+
+    printf("mean: %lf\n", (double) sum / n);
+    
+    return;
 }
