b3c480b3897dab0b9d339b93acc9f8e9f64a23c9
snowflake <snowflake111192@gmail.com>
2022-11-24 01:45:48 +0800

    hw0401

--- school/computer_programming1/hw4/41147019S_HW04/hw0401.c
+++ school/computer_programming1/hw4/41147019S_HW04/hw0401.c
@@ -4,8 +4,27 @@
 #include "myfunc.h"
 
 int main(){
-    int32_t coefficients[] = {12, 34, 56};
-    int32_t powers[] = {5, 2, 0};
+    int32_t coefficients[100] = {0};
+    int32_t powers[100] = {0};
+    int32_t n = 0;
+    double a = 0;
+    int32_t size = 0;
+    
+    printf("n: ");
+    scanf("%d", &n);
+    printf("a: ");
+    scanf("%lf", &a);
+    printf("size: ");
+    scanf("%d", &size);
 
-    printf("%lf\n", cal_n_derivatives(3, 2, 3, coefficients, powers));
+    for(int32_t i=0 ; i<size ; i++){
+        printf("coef %d: ", i+1);
+        scanf("%d", &coefficients[i]);
+    }
+    for(int32_t i=0 ; i<size ; i++){
+        printf("pow %d: ", i+1);
+        scanf("%d", &powers[i]);
+    }
+    
+    printf("%lf\n", cal_n_derivatives(n, a, size, coefficients, powers));
 }
