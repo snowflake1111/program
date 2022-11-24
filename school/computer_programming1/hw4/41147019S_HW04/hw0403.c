19898a5e02c9e41d6dc35f821bac74da4a4de9d4
SF <66545038+snowflake1111@users.noreply.github.com>
2022-11-24 16:16:37 +0800

    Update hw0403.c

--- school/computer_programming1/hw4/41147019S_HW04/hw0403.c
+++ school/computer_programming1/hw4/41147019S_HW04/hw0403.c
@@ -1,39 +1,114 @@
 #include <stdio.h>
 #include <stdint.h>
-#define RED "\x1B[0;31m"
-#define GRN "\x1B[0;32m"
-#define YEL "\x1B[0;33m"
-#define YEL_L "\x1B[1;33m"
-#define BLU "\x1B[0;34m"
-#define RESET "\x1B[0m"
-#define RED_L "\x1B[1;31m"
+#define RED "\x1B[0;31m"    //1
+#define RED_L "\x1B[1;31m"  //2
+#define GRN "\x1B[0;32m"    //3
+#define YEL "\x1B[0;33m"    //4
+#define BLU "\x1B[0;34m"    //5
+#define RESET "\x1B[0m"     //6
+
+void print(int32_t color[][12]);
 
 int main(){
-    int64_t state = 1;
-    
+    int32_t input = 0;
+    int32_t color[9][12] = {{0, 0, 0, 6, 6, 6, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 6, 6, 6, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 6, 6, 6, 0, 0, 0, 0, 0, 0},
+                            {3, 3, 3, 1, 1, 1, 5, 5, 5, 2, 2, 2},
+                            {3, 3, 3, 1, 1, 1, 5, 5, 5, 2, 2, 2},
+                            {3, 3, 3, 1, 1, 1, 5, 5, 5, 2, 2, 2},
+                            {0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0}};
+                                
     printf("Start:\n");
-    printf(RED"* * *\n");
-    printf(RED"* * *\n");
-    printf(RED"* * *\n");
-
-    while(1){
-        printf("Input:\n");
-        printf(RED"* * *\n");
-        printf(RED"* * *\n");
-        printf(RED"* * *\n");
-    }
-
-    printf(RED"*\n");
-    printf(RED_L"*\n");
-    printf(YEL"*\n");
-    printf(YEL_L"*\n");
-    printf(GRN"*\n");
-    printf(BLU"*\n");
-    printf(RESET"*\n");
+    print(color);
     
+    while(1){
+        printf(RESET"Input [0-6]: ");
+        scanf("%d", &input);
+        
+        if(input == 0){
+            break;
+        }
+        else if(input == 1 || input == 2 || input == 3){
+            int32_t temp1[3][3] = {{0}, {0}};
+            int32_t temp2[12] = {0};
+            
+            if(input == 1){
+                for(int32_t i=0 ; i<3 ; i++){
+                    for(int32_t j=0 ; j<3 ; j++){
+                        temp1[i][j] == color[i][j+3];
+                    }
+                }
+                for(int32_t i=0 ; i<3 ; i++){
+                    for(int32_t j=0 ; j<3 ; j++){
+                        temp1[i][j+3] == color[j][5-i];
+                    }
+                }
+            }
+            
+            for(int32_t i=0 ; i<12 ; i++){
+                temp2[i] = color[input+2][i];
+            }
+            for(int32_t i=0 ; i<12 ; i++){
+                color[input+2][i] = temp2;
+            }
+        }
+        else if(input == 4 || input == 5 || input == 6){
+            
+        }
+        else{
+            printf("Wrong input\n");
+            continue;
+        }
+    }
+    
+    return 0;
 }
-/*
-void print(){
-    switch(color)
+
+void print(int32_t color[][12]){
+    for(int32_t i=3 ; i<6 ; i++){
+        for(int32_t j=3 ; j<5 ; j++){
+            if(color[i][j] == 1){
+                printf(RED"* ");
+            }
+            else if(color[i][j] == 2){
+                printf(RED_L"* ");
+            }
+            else if(color[i][j] == 3){
+                printf(GRN"* ");
+            }
+            else if(color[i][j] == 4){
+                printf(YEL"* ");
+            }
+            else if(color[i][j] == 5){
+                printf(BLU"* ");
+            }
+            else if(color[i][j] == 6){
+                printf(RESET"* ");
+            }
+        }
+        
+        if(color[i][5] == 1){
+            printf(RED"*\n");
+        }
+        else if(color[i][5] == 2){
+            printf(RED_L"*\n");
+        }
+        else if(color[i][5] == 3){
+            printf(GRN"*\n");
+        }
+        else if(color[i][5] == 4){
+            printf(YEL"*\n");
+        }
+        else if(color[i][5] == 5){
+            printf(BLU"*\n");
+        }
+        else if(color[i][5] == 6){
+            printf(RESET"*\n");
+        }
+    }
+    
+    return;
 }
-*/
