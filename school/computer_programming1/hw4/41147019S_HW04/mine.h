f525cc5eaffd344f5edecfda6ea67ec7829c3863
SF <66545038+snowflake1111@users.noreply.github.com>
2022-11-26 05:11:40 +0800

    Add files via upload

---
+++ school/computer_programming1/hw4/41147019S_HW04/mine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
+void find_good_cells( const int32_t board[16][30], int32_t row [480], int32_t col[480] );
+
+void change_board(int32_t a, int32_t b, const int32_t board[16][30], int32_t real_board[16][30]);
+
+int32_t calculate(int32_t a, int32_t b, int32_t real_board[16][30], int32_t count[16][30], int32_t flag[16][30]);
