1a9809bf3107eecc41f9f7cc832c0cc806d4fc8b
snowflake <snowflake111192@gmail.com>
2022-11-28 19:13:46 +0800

    2022.11.28

--- school/computer_programming1/hw4/41147019S_HW04/hw0405.c
+++ school/computer_programming1/hw4/41147019S_HW04/hw0405.c
@@ -3,30 +3,41 @@
 #include "basic.h"
 
 void cell_calculate(int32_t i, int32_t j, int32_t ID1[5][5], int32_t ID2[5][5], int32_t point[5][5]);
+void step(int32_t i, int32_t j, int32_t ID1[5][5], int32_t ID2[5][5], int32_t point[5][5], int32_t final_point[70], int32_t temp, int32_t save, int32_t index, int32_t type[5][5], int32_t result_type[8][70]);
 
 int main(){
     int32_t point[5][5] = {{0}, {0}, {0}, {0}, {0}};
+    int32_t final_point[70] = {0};
+    int32_t result_type[8][70] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}};
     int32_t top1 = 0;
     int32_t top2 = 0;
     int32_t top3 = 0;
-    
+
     for(int32_t i=0 ; i<5 ; i++){
         for(int32_t j=0 ; j<5 ; j++){
-            cell_calculate(i, j, ID1, ID2);
+            cell_calculate(i, j, AttributeID1, AttributeID2, point);
         }
     }
     
-    for(int32_t i=0 ; i<5 ; i++){
-        
-    }
-    
-    
-        
-    
-    //判斷前三
-    
+    step(0, 1, AttributeID1, AttributeID2, point, final_point, 0, 0, 0, TypeOfAttribute, result_type);
+    step(1, 0, AttributeID1, AttributeID2, point, final_point, 0, 0, 0, TypeOfAttribute, result_type);
+
+
     //特殊規則
+    if()    
     
+
+    for(int32_t i=0 ; i<5 ; i++){
+        for(int32_t j=0 ; j<5 ; j++){
+            printf("%d", point[i][j]);
+        }
+    }
+
+    for(int32_t i=0 ; i<70 ; i++){
+        printf("%d", final_point[i]);
+    }
+
+    //判斷前三    
     
     
     for(int32_t i=0 ; i<3 ; i++){
@@ -69,6 +80,23 @@ void cell_calculate(int32_t i, int32_t j, int32_t ID1[5][5], int32_t ID2[5][5], 
     }
 }
 
-void arrange(int32_t top1, int32_t top2, int32_t top3){
-    int32_t temp = 0;
+void step(int32_t i, int32_t j, int32_t ID1[5][5], int32_t ID2[5][5], int32_t point[5][5], int32_t final_point[70], int32_t temp, int32_t save, int32_t index, int32_t type[5][5], int32_t result_type[8][70]){
+    temp = point[i][j];
+    result_type
+    
+    if(i < 4 && j < 4){
+        step(i+1, j, ID1, ID2, point, final_point, 0, save, index);
+        step(i, j+1, ID1, ID2, point, final_point, 0, save, index);
+    }
+    else if(i == 4 && j < 4){
+        step(i, j+1, ID1, ID2, point, final_point, 0, save, index);
+    }
+    else if(i == 4 && j < 4){
+        step(i+1, j, ID1, ID2, point, final_point, 0, save, index);
+    }
+    else{
+        final_point[index] = save;
+        index = index + 1;
+        save = save - temp;
+    }    
 }
