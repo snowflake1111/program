c978f5a082ece6bc4cacd75f45d623b8b41c8f8a
SF <66545038+snowflake1111@users.noreply.github.com>
2022-11-28 13:12:00 +0800

    Add files via upload

---
+++ school/computer_programming1/hw4/41147019S_HW04/hw0404.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+
+
+int main(){
+    int32_t mid_board[121] = {0};
+    int32_t board[17][13] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}};
+    int32_t color[17][13] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}};
+    
+    setup(board);
+    
+    while(1){
+        
+    }
+    
+    return 0;
+}
+
+setup(int32_t board[17][13]){
+    for(int32_t i=0 ; i<4 ; i++){
+        for(int32_t j=0 ; j<4 ; j++){
+            color[][];
+        }
+    }
+}
+
+print_board(int32_t board[17][13]){
+    for(){
+        
+    }
+}
